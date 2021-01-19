31bd862b9361d9896513e0ec46b8799cdd2305b8
Théo <12476264+theoricher@users.noreply.github.com>
2021-01-19 14:06:13 +0100

    Fix for silent install Tap Driver

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -24,5 +24,15 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 }
 
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+# Load custom functions
+. "$toolsDir\utils\utils.ps1"
+$trustedPublisherCertificate = "$toolsDir\openvpn_trusted_publisher.cer"
+$trustedPublisherCertificateHash = '8f53adb36f1c61c50e11b8bdbef8d0ffb9b26665a69d81246551a0b455e72ec0b26a34dc9b65cb3750baf5d8a6d19896c3b4a31b578b15ab7086377955509fad'
+Write-Host "Adding OpenVPN to the Trusted Publishers (needed to have a silent install of the TAP driver)..."
+AddTrustedPublisherCertificate -file "$trustedPublisherCertificate"
 
 Install-ChocolateyPackage @packageArgs
+
+Write-Host "Removing OpenVPN from the Trusted Publishers..."
+RemoveTrustedPublisherCertificate -file "$trustedPublisherCertificate"
